63b30f62811e51338e8358de47e2350a6b320ec3
kijeia <kidelajaneg@gmail.com>
2023-10-22 23:25:04 +0800

    feat: selected project - responsiveness

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,27 +1,95 @@
 import Image from 'next/image'
-import React, { useState } from 'react'
-import SampleProjects from './SampleData'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import SampleProjects, { ProjectSchema } from './SampleData'
 
 const Projects = () => {
-  const [selectedProject, setSelectedProject] = useState<string>('')
+  const [selectedProject, setSelectedProject] = useState<ProjectSchema | null>(
+    null
+  )
+  const [isProjectVisible, setIsProjectVisible] = useState(false)
+
+  useEffect(() => {
+    // Simulate some asynchronous operation, like fetching data
+    setTimeout(() => {
+      setIsProjectVisible(true)
+    }, 1000) // Adjust the duration as needed
+  }, [selectedProject?.name])
+
+  const handleLink = (p: ProjectSchema) => {
+    if (selectedProject?.name === p?.name) setSelectedProject(null)
+    else setSelectedProject(p)
+    setIsProjectVisible(false)
+  }
 
   return (
     <div className='projects'>
+      {/* SELECTED PROJECT */}
+
+      <div
+        className={`selected-project ${isProjectVisible ? '--visible' : ''}`}
+      >
+        {selectedProject !== null ? (
+          <>
+            {/* Selected Project */}
+            <div className='__title'>
+              <Image
+                src='/assets/img/bookmark.png'
+                alt='email'
+                width={32}
+                height={32}
+              />
+              <span>{selectedProject?.name}</span>
+            </div>
+            <p className='__description'>{selectedProject?.description}</p>
+            <div className='__bottom'>
+              <div className='__technologies'>
+                <h1>Key Technologies: </h1>
+                {selectedProject?.keyTechnologies?.map((stack, i) => (
+                  <div key={stack}>
+                    {stack}
+                    {i + 1 < selectedProject?.keyTechnologies?.length! && ','}
+                  </div>
+                ))}
+              </div>
+              <div className='__links'>
+                {selectedProject?.links?.map(({ label, link }) => (
+                  <Link key={label} href={link}>
+                    {label}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className='__welcome'>
+            {/* Welcome Message */}
+
+            <span>Welcome</span>
+            {`to my digital playground! I'm a React JS and React Native enthusiast, weaving pixels into delightful user experiences. With an iced coffee in hand and a passion for clean code, I bring ideas to life. Explore the bits and bytes of my portfolio, where each line of code tells a story. Let's build something awesome together!`}
+          </div>
+        )}
+      </div>
+
+      <hr />
+
       {/* PROJECTS */}
       <div className='projects-container'>
         {/* List */}
         <div className='projects-sub-container'>
           <h1>Projects</h1>
           <div className='list'>
-            {SampleProjects?.map(({ name }) => (
-              <div key={name} className='name'>
+            {SampleProjects?.map((p) => (
+              <div key={p.name} className='name'>
                 <button
-                  id={`project-${name}`}
+                  id={`project-${p.name}`}
                   type='button'
-                  className={selectedProject === name ? '--selected' : ''}
-                  onClick={() => setSelectedProject(name)}
+                  className={
+                    selectedProject?.name === p.name ? '--selected' : ''
+                  }
+                  onClick={() => handleLink(p)}
                 >
-                  {name}
+                  {p.name}
                 </button>
               </div>
             ))}
--- components/Projects/SampleData.tsx
+++ components/Projects/SampleData.tsx
@@ -1,17 +1,36 @@
-const SampleProjects = [
+export interface ProjectSchema {
+  name: string
+  description: string
+  links: Array<{ label: string; link: string }>
+  keyTechnologies?: Array<string>
+}
+
+const SampleProjects: ProjectSchema[] = [
   {
     name: 'Sample 1',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
+    ],
+    keyTechnologies: [
+      'React JS',
+      'React Native',
+      'Tailwind CSS',
+      'Ant Design (Antd)',
+      'Storybook',
+      'Zustand',
+      'Tiptap Integration',
+      'i18n Translation',
+      'Weglot Translation',
+      'Stripe Integration'
     ]
   },
   {
     name: 'Sample 2 Lorem',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -20,7 +39,7 @@ const SampleProjects = [
   {
     name: 'Sample 3',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -29,7 +48,7 @@ const SampleProjects = [
   {
     name: 'Sample 4',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -38,7 +57,7 @@ const SampleProjects = [
   {
     name: 'Sample 5',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -47,7 +66,7 @@ const SampleProjects = [
   {
     name: 'Sample 6',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -56,7 +75,7 @@ const SampleProjects = [
   {
     name: 'Sample 7',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
@@ -65,7 +84,7 @@ const SampleProjects = [
   {
     name: 'Sample 8',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Sample 8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
     links: [
       { label: 'View Site', link: '/test.com' },
       { label: 'Github', link: '/test.com' }
