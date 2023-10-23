f10d5fd4c384b24886131a59ac6bee9309c0873a
kijeia <kidelajaneg@gmail.com>
2023-10-23 18:00:54 +0800

    feat: projects data

--- components/Projects/SampleData.tsx
+++ components/Projects/SampleData.tsx
@@ -7,19 +7,20 @@ export interface ProjectSchema {
 
 const SampleProjects: ProjectSchema[] = [
   {
-    name: 'Sample 1',
+    name: 'Betterteem Web-App',
     description:
-      'Sample 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Betterteem AI® enables organizations to predict and mitigate unwanted employee resignations.',
     links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
+      { label: 'View Web App', link: 'https://go.betterteem.com/auth/login' },
+      { label: 'View Site', link: 'https://www.betterteem.com' }
     ],
     keyTechnologies: [
       'React JS',
-      'React Native',
+      'TypeScript',
       'Tailwind CSS',
       'Ant Design (Antd)',
       'Storybook',
+      'Redux',
       'Zustand',
       'Tiptap Integration',
       'i18n Translation',
@@ -28,67 +29,53 @@ const SampleProjects: ProjectSchema[] = [
     ]
   },
   {
-    name: 'Sample 2 Lorem',
+    name: 'Betterteem Mobile-App',
     description:
-      'Sample 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'Betterteem AI® enables organizations to predict and mitigate unwanted employee resignations.',
     links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
+      {
+        label: 'IOS',
+        link: 'https://apps.apple.com/ph/app/betterteem-play/id6447600533'
+      },
+      {
+        label: 'Android',
+        link: 'https://apkcombo.com/betterteem-play/com.betterteem.mobile/'
+      }
+    ],
+    keyTechnologies: [
+      'React Native',
+      'TypeScript',
+      'Tailwind CSS',
+      'i18n Translation',
+      'Zustand',
+      'CLI'
     ]
   },
   {
-    name: 'Sample 3',
-    description:
-      'Sample 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
-    links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
+    name: 'Taxikel',
+    description: 'Get deliveries and run errands from your device.',
+    links: [{ label: 'View Site', link: 'https://www.taxikel.com' }],
+    keyTechnologies: ['React JS', 'JavaScript', 'CSS', 'Redux']
   },
   {
-    name: 'Sample 4',
+    name: 'NALA - Neurodermitis App',
     description:
-      'Sample 4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
+      'NALA has been developed by a dedicated team, including experts in dermatology, psychology, integrative medicine, sleep, and nutrition, all with a deep understanding of eczema.',
     links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
+      { label: 'View Site', link: 'https://nala.care/en/' },
+      {
+        label: 'View Mobile App',
+        link: 'https://apps.apple.com/ph/app/eczema-app-nala/id1545319742'
+      }
+    ],
+    keyTechnologies: ['React Native', 'JavaScript', 'CSS', 'i18n Translation']
   },
   {
-    name: 'Sample 5',
+    name: 'Edusuite Mobile-App',
     description:
-      'Sample 5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
-    links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
-  },
-  {
-    name: 'Sample 6',
-    description:
-      'Sample 6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
-    links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
-  },
-  {
-    name: 'Sample 7',
-    description:
-      'Sample 7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
-    links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
-  },
-  {
-    name: 'Sample 8',
-    description:
-      'Sample 8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ac justo eu efficitur. Nullam facilisis velit ut dapibus cursus. Vivamus varius, ipsum at ultricies cursus, lectus nisl fringilla tellus, vel vehicula augue turpis id dolor. Fusce nec luctus elit. In hac habitasse platea dictumst. Phasellus et justo id est venenatis varius eu eu nisi. Proin vel neque vel dui aliquet bibendum.',
-    links: [
-      { label: 'View Site', link: '/test.com' },
-      { label: 'Github', link: '/test.com' }
-    ]
+      'Edusuite’s AI-driven technology analyzes data and resources, enabling school administrators, faculty, and students to enhance resource allocation and make informed decisions.',
+    links: [{ label: 'View Site', link: 'https://www.edusuite.asia' }],
+    keyTechnologies: ['React Native', 'CSS', 'JavaScript', 'Redux']
   }
 ]
 
